ed4c8fa141ea285944a871859d163ce558430aaa
husky <fekhesk@pm.me>
2022-04-25 14:09:43 -0700

    did something idk

--- src/types.rs
+++ src/types.rs
@@ -1,9 +1,10 @@
 use x11::xlib::Window;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct CumWindow {
     pub x: i32,
     pub y: i32,
     pub width: i32,
     pub height: i32,
-    pub WindowID: Window,
+    pub window_id: Window,
 }
