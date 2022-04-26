db8d12589d2994752c7888f7163b3300ad12acaf
husky <fekhesk@pm.me>
2022-04-26 14:09:44 -0700

    converting to xcb cause (:

--- src/main.rs
+++ src/main.rs
@@ -1,139 +1,107 @@
 mod types;
-use std::os::raw::{c_int, c_ulong};
+
+use std::borrow::Borrow;
+use std::ffi::CStr;
+use std::os::raw::{c_char, c_int, c_ulong};
 use std::ptr::{null, null_mut};
 use std::time::SystemTime;
-use x11::xlib::*;
+use xcb::{x, Xid};
 use crate::types::CumWindow;
 
+/*
+unsafe extern "C" fn error_handler(display: *mut Display, error_event: *mut XErrorEvent) -> c_int {
+    unsafe { println!("X Error: {}", (*error_event).error_code); }
+    0
+}
+ */
 
 fn main() {
-    let display = unsafe { XOpenDisplay(null()) }; // get ready for a ton of unsafe code! (:
-    if display.is_null() {
-        panic!("Failed to open display");
-    }
-    let screen = unsafe { XDefaultScreen(display) };
-    let mut attr: *mut XWindowAttributes = &mut XWindowAttributes{
-        x: 0,
-        y: 0,
-        width: 0,
-        height: 0,
-        border_width: 0,
-        depth: 0,
-        visual: null_mut() as *mut Visual,
-        root: 0,
-        class: 0,
-        bit_gravity: 0,
-        win_gravity: 0,
-        backing_store: 0,
-        backing_planes: 0,
-        backing_pixel: 0,
-        save_under: 0,
-        colormap: 0,
-        map_installed: 0,
-        map_state: 0,
-        all_event_masks: 0,
-        your_event_mask: 0,
-        do_not_propagate_mask: 0,
-        override_redirect: 0,
-        screen: null_mut() as *mut Screen,
-    } as *mut XWindowAttributes;
-    let ev : *mut XEvent = &mut XEvent{
-        pad: [0; 24],
-    } as *mut XEvent;
-    let root = unsafe { XDefaultRootWindow(display) };
+    let (conn, screen_num) = xcb::Connection::connect(None).expect("Failed to connect to X server");
+    let setup = conn.get_setup();
+    let screen = setup.roots().nth(screen_num as usize).unwrap();
+    // get root window
+    let root = screen.root();
 
-    let mut src_width = unsafe { XDisplayWidth(display, screen) };
-    let mut src_height = unsafe { XDisplayHeight(display, screen) };
+    // get dimensions
+    let mut src_width = screen.width_in_pixels();
+    let mut src_height = screen.height_in_pixels();
 
-    let mut windows : Vec<types::CumWindow> = Vec::new();
-
-    // visual
-    let visual = unsafe { XDefaultVisual(display, screen) };
-
-    // colormap
-    let mut colormap = unsafe { XCreateColormap(display, root, visual, AllocNone) };
-    // install colormap
-    unsafe { XInstallColormap(display, colormap) };
+    let mut windows: Vec<types::CumWindow> = Vec::new();
 
     let mut accent_color = 0xFFFF0000;
 
     // get root window and set attributes so we receive events
-    let mut set_attr: *mut XSetWindowAttributes = &mut XSetWindowAttributes{
-        background_pixmap: 0,
-        background_pixel: 0,
-        border_pixmap: 0,
-        border_pixel: accent_color,
-        bit_gravity: 0,
-        win_gravity: 0,
-        backing_store: 0,
-        backing_planes: 0,
-        backing_pixel: 0,
-        save_under: 0,
-        event_mask: 0,
-        do_not_propagate_mask: 0,
-        override_redirect: 0,
-        colormap,
-        cursor: 0
-    } as *mut XSetWindowAttributes;
-    unsafe {
-        (*set_attr).event_mask = StructureNotifyMask|SubstructureNotifyMask|EnterWindowMask|LeaveWindowMask;
-        XChangeWindowAttributes(display, root, CWEventMask, set_attr)
-    };
+    // gotta enable events in the
+    let cookie = conn.send_request_checked(&x::ChangeWindowAttributes {
+        window: root,
+        value_list: &[
+            x::Cw::EventMask(
+                x::EventMask::BUTTON_PRESS |
+                x::EventMask::BUTTON_RELEASE |
+                x::EventMask::KEY_PRESS |
+                x::EventMask::KEY_RELEASE |
+                //x::EventMask::EXPOSURE |
+                x::EventMask::SUBSTRUCTURE_NOTIFY |
+                //x::EventMask::SUBSTRUCTURE_REDIRECT
+                x::EventMask::STRUCTURE_NOTIFY
+            )
+        ],});
+
+    let checked = conn.check_request(cookie);
+    if checked.is_err() {
+        println!("Error setting event mask, is another window manager running?");
+    }
+
+    conn.flush();
 
     let mut now = SystemTime::now();
     let mut t = 0;
 
-
     loop {
-        let event_pending = unsafe { XPending(display) };
-        if (event_pending > 0) {
-            unsafe { XNextEvent(display, ev); }
-            if ev.is_null() {
-                panic!("Failed to get next event");
-            }
-            unsafe {
-                match (*ev).type_ {
-                    CreateNotify => {
+        let event_pending = conn.poll_for_event();
+        // if we have an event
+        if let Ok(event_success) = event_pending {
+            if event_success.is_some() {
+                println!("{:?}", event_success);
+                let event = conn.wait_for_event();
+                match event.unwrap() {
+                    xcb::Event::X(x::Event::CreateNotify(ev)) => {
+                        // set border width
+                        conn.send_request(&x::ConfigureWindow {
+                            window: ev.window(),
+                            value_list: &[x::ConfigWindow::BorderWidth(5)],
+                        });
                         // check the parent window to see if it's the root window
-                        let create_notify: XCreateWindowEvent = (*ev).try_into().expect("Failed to convert event");
-                        unsafe {
-                            //XSelectInput(display, create_notify.window, (*set_attr).event_mask);
-                            XSetWindowBorderWidth(display, create_notify.window, 5);
-                            // set border color
-                            XChangeWindowAttributes(display, create_notify.window, CWBorderPixel, set_attr);
-                            XSetWindowBorder(display, create_notify.window, 0xFFFFFF00);
-                        };
-                        if root != create_notify.parent {
-                            // child window, ignore
+                        if root != ev.parent() {
                             continue;
                         }
                         windows.push(CumWindow {
-                            window_id: create_notify.window,
-                            x: create_notify.x,
-                            y: create_notify.y,
-                            width: create_notify.width,
-                            height: create_notify.height,
+                            window_id: ev.window(),
+                            x: ev.x(),
+                            y: ev.y(),
+                            width: ev.width(),
+                            height: ev.height(),
+                            is_opening: false,
+                            animation_time: 0
                         });
                     },
-                    DestroyNotify => {
-                        let destroy_notify: XDestroyWindowEvent = (*ev).try_into().expect("Failed to convert event");
-                        windows.retain(|w| w.window_id != destroy_notify.window);
+                    xcb::Event::X(x::Event::DestroyNotify(ev)) => {
+                        windows.retain(|w| w.window_id != ev.window());
                     },
-                    ConfigureNotify => {
-                        let configure_notify: XConfigureEvent = (*ev).try_into().expect("Failed to convert event");
+                    xcb::Event::X(x::Event::ConfigureNotify(ev)) => {
                         // check if the window is the root window
-                        if configure_notify.window == root {
-                            src_height = configure_notify.height;
-                            src_width = configure_notify.width;
+                        if ev.window() == root {
+                            src_height = ev.height();
+                            src_width = ev.width();
                         }
                         let mut found = false;
                         for w in windows.iter_mut() {
-                            if w.window_id == configure_notify.window {
+                            if w.window_id == ev.window() {
                                 found = true;
-                                w.x = configure_notify.x;
-                                w.y = configure_notify.y;
-                                w.width = configure_notify.width;
-                                w.height = configure_notify.height;
+                                w.x = ev.x();
+                                w.y = ev.y();
+                                w.width = ev.width();
+                                w.height = ev.height();
                                 break;
                             }
                         }
@@ -145,14 +113,8 @@ fn main() {
                     _ => {}
                 }
             }
-            // print windows
-            /*println!("{:?}", windows);
-            println!("{:?}", src_width);
-            println!("{:?}", src_height);
-             */
-        }
-        let after = SystemTime::now();
-        unsafe {
+
+            let after = SystemTime::now();
             if after.duration_since(now).unwrap().as_millis() > 10 {
                 // generate the rainbow using a sine wave
                 let frequency = 0.05;
@@ -164,18 +126,18 @@ fn main() {
                 t += 1;
                 // for each window, move it by 1 up and 1 right
                 for w in windows.iter_mut() {
-                    unsafe {
-                        XMoveWindow(display, w.window_id, w.x, w.y);
-                        XSynchronize(display, 0);
-                        XSetWindowBorder(display, w.window_id, accent_color);
-                    }
+                    // change the window's position
+                    conn.send_request(&x::ConfigureWindow {
+                        window: w.window_id,
+                        value_list: &[x::ConfigWindow::X(w.x as i32), x::ConfigWindow::Y(w.y as i32)],
+                    });
                     w.x += 1;
                     w.y += 1;
-                    if w.x >= src_width {
-                        w.x = 0 - w.width;
+                    if w.x >= src_width as i16 {
+                        w.x = 0 - w.width as i16;
                     }
-                    if w.y >= src_height {
-                        w.y = 0 - w.height;
+                    if w.y >= src_height as i16 {
+                        w.y = 0 - w.height as i16;
                     }
                 }
                 now = after;
--- src/types.rs
+++ src/types.rs
@@ -1,10 +1,12 @@
-use x11::xlib::Window;
+use xcb::x::Window;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct CumWindow {
-    pub x: i32,
-    pub y: i32,
-    pub width: i32,
-    pub height: i32,
+    pub x: i16,
+    pub y: i16,
+    pub width: u16,
+    pub height: u16,
     pub window_id: Window,
+    pub is_opening: bool,
+    pub animation_time: i32,
 }
