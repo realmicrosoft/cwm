5d4f4dfbeb951b8acbb93fbec8d1b1a25cdf81ee
husky <fekhesk@pm.me>
2022-04-28 12:27:58 -0700

    display events?

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
+use std::borrow::{Borrow, BorrowMut};
+use std::cell::RefCell;
+use std::fs::{File, OpenOptions};
+use std::os::unix::io::{AsRawFd, RawFd};
+use std::rc::Rc;
+use std::sync::Mutex;
+use smithay::backend::drm;
+use smithay::backend::drm::DrmDevice;
 use smithay::backend::input::{InputBackend, InputEvent};
 use smithay::backend::udev::UdevBackend;
 use smithay::backend::winit::init;
-use smithay::reexports::calloop::{EventLoop, LoopHandle};
+use smithay::reexports;
+use smithay::reexports::calloop::{EventLoop, Interest, LoopHandle, Mode, PostAction};
+use smithay::reexports::calloop::generic::Generic;
 use smithay::reexports::udev::Udev;
 use smithay::reexports::wayland_server::Display;
 
@@ -17,13 +27,24 @@ unsafe extern "C" fn error_handler(display: *mut Display, error_event: *mut XErr
 }
  */
 
+#[derive(Clone)]
+struct FdWrapper {
+    file: Rc<File>,
+}
+
+impl AsRawFd for FdWrapper {
+    fn as_raw_fd(&self) -> RawFd {
+        self.file.as_raw_fd()
+    }
+}
+
 struct CumBackend {
     winit_backend: Option<smithay::backend::winit::WinitGraphicsBackend>,
     winit_input_backend: Option<smithay::backend::winit::WinitInputBackend>,
 }
 
 struct Cum {
-    display: Display,
+    display: Rc<RefCell<Display>>,
     event_loop: LoopHandle<'static, Cum>,
     backend: CumBackend,
 }
@@ -31,8 +52,30 @@ struct Cum {
 fn main() {
     let mut event_loop: EventLoop<Cum> = EventLoop::try_new().expect("cant create event loop ):");
     let handle = event_loop.handle();
+
+    let mut display = Display::new();
+
+    let socket = display.add_socket_auto();
+
+    let display_fd = display.get_poll_fd();
+
+    handle.insert_source(
+        Generic::from_fd(display_fd, Interest::READ, Mode::Level),
+        move |_, _, state: &mut Cum| {
+            let mut display = state.display.clone();
+            let mut display = (*display).borrow_mut();
+            match display.dispatch(std::time::Duration::from_millis(0), state) {
+                Ok(_) => Ok(PostAction::Continue),
+                Err(e) => {
+                    println!("display error");
+                    Err(e)
+                }
+            }
+        },
+    ).expect("cant insert display fd");
+
     let mut cum = Cum {
-        display: Display::new(),
+        display: Rc::new(RefCell::new(display)),
         event_loop: handle,
         backend: CumBackend {
             winit_backend: None,
@@ -40,15 +83,10 @@ fn main() {
         },
     };
 
-    let (backend, input_backend) = init(None).unwrap();
-    cum.backend.winit_backend = Some(backend);
-    cum.backend.winit_input_backend = Some(input_backend);
-
     event_loop.run(
         std::time::Duration::from_millis(16),
         &mut cum,
         |_cum| {
-
         },
     ).expect("cant run event loop");
 }
