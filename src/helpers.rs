21d8b505a3c46c8ab85617e7ac92e4748bd7ff90
husky <husky@gaycatboy.sex>
2022-04-29 17:22:33 -0700

    oh god i wrote opengl code oh shit oh fuck

--- src/helpers.rs
+++ src/helpers.rs
@@ -17,10 +17,18 @@ pub fn rgba_to_bgra(rgba_array: &[u8]) -> Vec<u8> {
 
 }
 
+use std::ffi::c_void;
+use rogl::bitflags::{GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT};
+use rogl::command::PFN_glEnable;
+use rogl::enums::{GL_ARRAY_BUFFER, GL_BGRA, GL_FALSE, GL_FLOAT, GL_LINEAR, GL_MODULATE, GL_QUADS, GL_RGB, GL_RGBA, GL_STATIC_DRAW, GL_TEXTURE_2D, GL_TEXTURE_ENV_MODE, GL_TEXTURE_MAG_FILTER, GL_TEXTURE_MIN_FILTER, GL_UNSIGNED_BYTE};
+use rogl::gl::context::GLContext;
+use rogl::gl::gl40::GL40;
+use rogl::gl::gl33::GL33;
 use xcb::Connection;
 use xcb::{Xid};
+use crate::CumWindow;
 
-pub fn allow_input_passthrough(&conn: &Connection, window: xcb::x::Window, p_id: xcb::render::Picture,x: i16, y: i16) -> xcb::xfixes::Region {
+pub fn allow_input_passthrough(conn: &Connection, window: xcb::x::Window, p_id: xcb::render::Picture,x: i16, y: i16) -> xcb::xfixes::Region {
     // create copy of window bounding region
     let r_id = conn.generate_id();
     conn.send_request(&xcb::xfixes::CreateRegionFromWindow {
@@ -48,15 +56,96 @@ pub fn allow_input_passthrough(&conn: &Connection, window: xcb::x::Window, p_id:
     r_id
 }
 
-pub fn glx_pixmap_from_x(conn: &Connection, x_pixmap: xcb::x::Pixmap, screen_num: u32) -> xcb::glx::Pixmap {
-    let glx_pixmap = conn.generate_id();
-    conn.send_request(&xcb::glx::CreatePixmap {
-        screen: screen_num,
-        fbconfig: xcb::glx::Fbconfig::none(),
-        pixmap: x_pixmap,
-        glx_pixmap,
-        num_attribs: 0,
-        attribs: &[],
+use rogl::gl::gl45::GL45;
+use rogl::types::{GLboolean, GLfloat, GLint, GLsizei, GLsizeiptr, GLuint, GLvoid};
+
+trait GL: GL45 {}
+impl GL for GLContext {}
+
+pub fn draw_x_window<T>(conn: &Connection, window: CumWindow, ctx: &T) where T: GL {
+    unsafe {
+        let mut texture: GLuint = 0;
+        ctx.glEnable(GL_TEXTURE_2D);
+        ctx.glGenTextures(GL_TEXTURE_2D as GLsizei, &mut texture);
+        ctx.glBindTexture(GL_TEXTURE_2D, texture);
+        ctx.glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR as GLfloat);
+        ctx.glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR as GLfloat);
+        ctx.glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_ENV_MODE, GL_MODULATE as GLfloat);
+
+        let cookie = conn.send_request(&xcb::x::GetImage{
+            format: xcb::x::ImageFormat::ZPixmap,
+            drawable: xcb::x::Drawable::Window(window.window_id),
+            x: 0,
+            y: 0,
+            width: window.width,
+            height: window.height,
+            plane_mask: 0xffffffff,
+        });
+
+        let reply = conn.wait_for_reply(cookie);
+        let image = reply.unwrap().data();
+        let image_vec: Vec<u8> = Vec::from(image);
+
+
+        ctx.glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB as GLint,
+                         window.width as GLsizei, window.height as GLsizei,
+                         0, GL_BGRA, GL_UNSIGNED_BYTE,  image_vec.as_ptr() as *const c_void);
+
+        ctx.glViewport(0, 0, window.width as GLsizei, window.height as GLsizei);
+        ctx.glClearColor(0.3, 0.0, 0.3, 1.0);
+        ctx.glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        let vertices: [GLfloat; 8] = [
+            -1.0, -1.0,
+            1.0, -1.0,
+            1.0, 1.0,
+            -1.0, 1.0,
+        ];
+        let mut vbo_buffers: [GLuint; 1] = [0];
+        ctx.glCreateBuffers(1, &mut vbo_buffers[0]);
+        ctx.glBindBuffer(GL_ARRAY_BUFFER, vbo_buffers[0]);
+        ctx.glBufferData(GL_ARRAY_BUFFER,
+                         8 * std::mem::size_of::<GLfloat>() as GLsizeiptr,
+                         vertices.as_ptr() as *const c_void,
+                         GL_STATIC_DRAW);
+
+        let mut vao_buffers: [GLuint; 1] = [0];
+        ctx.glCreateVertexArrays(1, &mut vao_buffers[0]);
+        ctx.glBindVertexArray(vao_buffers[0]);
+        ctx.glEnableVertexAttribArray(0);
+        ctx.glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE as GLboolean, 0, std::ptr::null());
+
+        // draw
+        ctx.glBindVertexArray(vao_buffers[0]);
+        ctx.glBindTexture(GL_TEXTURE_2D, texture);
+        ctx.glDrawArrays(GL_QUADS, 0, 4);
+        ctx.glDeleteVertexArrays(1, &mut vao_buffers[0]);
+        ctx.glDeleteBuffers(1, &mut vbo_buffers[0]);
+        ctx.glDeleteTextures(1, &mut texture);
+    }
+}
+
+pub unsafe fn create_sdl2_context(src_width: u16, src_height: u16) -> (
+    GLContext,
+    sdl2::video::Window,
+    sdl2::EventPump,
+    sdl2::video::GLContext,
+) {
+    let sdl = sdl2::init().unwrap();
+    let video = sdl.video().unwrap();
+    let gl_attr = video.gl_attr();
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(3, 0);
+    let window = video
+        .window("CHAOTIC WINDOW MANAGER", src_width as u32, src_height as u32)
+        .opengl()
+        .build()
+        .unwrap();
+    let gl_context = window.gl_create_context().unwrap();
+    let context = GLContext::load(|s| {
+        video.gl_get_proc_address(s.to_str().expect("failed to conver string")) as *const _
     });
-    glx_pixmap
+    let event_loop = sdl.event_pump().unwrap();
+
+    (context, window, event_loop, gl_context)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod types;
 mod helpers;
 mod linkedlist;
+mod setup;
 
 use std::num::NonZeroU32;
 use std::os::raw::{c_ulong};
@@ -10,6 +11,7 @@ use fast_image_resize as fr;
 use xcb::{composite, Connection, glx, x, Xid};
 use crate::types::CumWindow;
 use crate::helpers::{allow_input_passthrough, glx_pixmap_from_x, rgba_to_bgra};
+use crate::setup::{setup_compositing, setup_desktop};
 
 fn redraw_desktop(conn: &Connection, pic_id: xcb::render::Picture, desktop_pic_id: xcb::render::Picture, src_width: u16, src_height: u16) {
     let cookie = conn.send_request_checked(&xcb::render::Composite {
@@ -130,125 +132,13 @@ fn main() {
         println!("Error setting event mask, is another window manager running?");
     }
 
-    // query version of composite extension so that we get a panic early on if it's not available
-    conn.send_request(&xcb::composite::QueryVersion {
-        client_major_version: 1,
-        client_minor_version: 0,
-    });
-
-    // redirect subwindows of root window
-    let cookie = conn.send_request_checked(&xcb::composite::RedirectSubwindows {
-        window: root,
-        update: composite::Redirect::Manual,
-    });
-
-    let checked = conn.check_request(cookie);
-    if checked.is_err() {
-        println!("Error redirecting subwindows, is another window manager running?");
-    }
-
-    // get pictformat
-    let pict_format_cookie = conn.send_request(&xcb::render::QueryPictFormats {});
-    let pict_format_reply = conn.wait_for_reply(pict_format_cookie);
-    // go through all pictformats to find a suitable one
-    let mut pict_format: xcb::render::Pictformat = xcb::render::Pictformat::none();
-    for pict_format_reply in pict_format_reply.unwrap().formats() {
-        if pict_format_reply.depth() == 24 {
-            pict_format = pict_format_reply.id();
-            break;
-        }
-    }
-
-    // enable bigreq extension
-    let cookie = conn.send_request(&xcb::bigreq::Enable {});
-
-    let reply = conn.wait_for_reply(cookie);
-    if reply.is_err() {
-        println!("Error enabling bigreq extension");
-    }
-    // check maximum request size
-    println!("Maximum request size: {}", reply.unwrap().maximum_request_length());
-
-    // get overlay window
-    let cookie = conn.send_request(&xcb::composite::GetOverlayWindow {
-        window: root,
-    });
-
-    let reply = conn.wait_for_reply(cookie);
-    if reply.is_err() {
-        println!("Error getting overlay window");
-    }
-
-    let overlay_window = reply.unwrap().overlay_win();
-
-    // get overlay picture
-    let r_id = conn.generate_id();
-    conn.send_request(&xcb::render::CreatePicture {
-        pid: r_id,
-        drawable: x::Drawable::Window(overlay_window),
-        format: pict_format,
-        value_list: &[
-            xcb::render::Cp::SubwindowMode(xcb::x::SubwindowMode::IncludeInferiors),
-        ],
-    });
-
-    // allow input passthrough
-    allow_input_passthrough(&conn, overlay_window, r_id, 0, 0);
-
-    // create glx context
-    let ctx = conn.generate_id();
-    conn.send_request(&xcb::glx::CreateContext{
-        context: ctx,
-        visual: screen.root_visual(),
-        screen: screen_num as u32,
-        share_list : xcb::glx::Context::none(),
-        is_direct: false,
-    });
-
-    // create the glx window
-    let stage = conn.generate_id();
-    conn.send_request(&xcb::glx::CreateWindow{
-        screen: screen_num as u32,
-        fbconfig: xcb::glx::Fbconfig::none(),
-        window: overlay_window,
-        glx_window: stage,
-        num_attribs: 0,
-        attribs: &[],
-    });
-
-    // make current
-    let ctx_tag = 0u32;
-    conn.send_request(&xcb::glx::MakeCurrent{
-        drawable: xcb::glx::Drawable::Window(stage),
-        context: ctx,
-        old_context_tag: ctx_tag,
-    });
-
-    // create new window for desktop
-    let desktop_id = conn.generate_id();
-    conn.send_request(&x::CreateWindow {
-        depth: x::COPY_FROM_PARENT as u8,
-        wid: desktop_id,
-        parent: root,
-        x: 0,
-        y: 0,
-        width: src_width,
-        height: src_height,
-        border_width: 0,
-        class: x::WindowClass::InputOutput,
-        visual: screen.root_visual(),
-        value_list: &[
-            x::Cw::EventMask(x::EventMask::EXPOSURE),
-        ],
-    });
-
-    conn.flush().expect("Could not flush");
+    let (overlay_window, pict_format) = setup_compositing(&conn, root);
 
     let gcon_id: x::Gcontext = conn.generate_id();
     // create a graphics context
     let cookie = conn.send_request_checked(&xcb::x::CreateGc {
         cid: gcon_id,
-        drawable: x::Drawable::Window(desktop_id),
+        drawable: x::Drawable::Window(root),
         value_list: &[
             x::Gc::Foreground(accent_color),
             x::Gc::GraphicsExposures(true)
@@ -260,174 +150,7 @@ fn main() {
         println!("Error creating graphics context");
     }
 
-    // create glx context
-    let glx_id = conn.generate_id();
-    let cookie = conn.send_request_checked(&glx::CreateContext {
-        context: glx_id,
-        visual: screen.root_visual(),
-        screen: screen_num as u32,
-        share_list: glx::Context::none(),
-        is_direct: true,
-    });
-
-    let checked = conn.check_request(cookie);
-    if checked.is_err() {
-        println!("Error creating glx context");
-    }
-
-    // load the bg.png image
-    let bg_image_load = stb_image::image::load("bg.png");
-    let bg_image = match bg_image_load {
-        LoadResult::ImageU8(image) => image,
-        LoadResult::ImageF32(_) => panic!("bg.png is not 8-bit"),
-        LoadResult::Error(e) => panic!("Error loading bg.png: {}", e),
-    };
-
-    let bg_image_width = NonZeroU32::new(bg_image.width as u32).unwrap();
-    let bg_image_height = NonZeroU32::new(bg_image.height as u32).unwrap();
-
-    let mut divide_factor = 1;
-    let mut potential_size: u32 = (src_width / divide_factor) as u32 * (src_height / divide_factor) as u32;
-    // calculate the amount of bytes that src_width * src_height * 4 will take
-    while potential_size > 300000 {
-        divide_factor += 1;
-        potential_size = (src_width / divide_factor) as u32 * (src_height / divide_factor) as u32;
-    }
-
-    let mut src = fr::Image::from_vec_u8(
-        bg_image_width,
-        bg_image_height,
-        bg_image.data.clone(),
-        fr::PixelType::U8x4,
-    ).unwrap();
-    // Create MulDiv instance
-    let alpha_mul_div = fr::MulDiv::default();
-    // Multiple RGB channels of source image by alpha channel
-    alpha_mul_div
-        .multiply_alpha_inplace(&mut src.view_mut())
-        .unwrap();
-
-    let dst_width = NonZeroU32::new((src_width / divide_factor) as u32).unwrap();
-    let dst_height = NonZeroU32::new((src_height / divide_factor) as u32).unwrap();
-    let mut dst_image = fr::Image::new(
-        dst_width,
-        dst_height,
-        fr::PixelType::U8x4,
-    );
-    let mut dst_view = dst_image.view_mut();
-
-    let mut resizer = fr::Resizer::new(
-        fr::ResizeAlg::Convolution(fr::FilterType::Lanczos3),
-    );
-    resizer.resize(&src.view(), &mut dst_view).unwrap();
-
-    alpha_mul_div.divide_alpha_inplace(&mut dst_view).unwrap();
-
-    let bg_image_data = dst_image.buffer();
-
-    // create a pixmap to draw on
-    let bg_id = conn.generate_id();
-    let cookie = conn.send_request_checked(&xcb::x::CreatePixmap {
-        depth: 24,
-        pid: bg_id,
-        drawable: x::Drawable::Window(desktop_id),
-        width: (src_width / divide_factor) as u16,
-        height: (src_height / divide_factor) as u16,
-    });
-
-    let checked = conn.check_request(cookie);
-    if checked.is_err() {
-        println!("Error creating pixmap");
-        println!("{:?}", checked);
-    }
-
-    // put the image on the pixmap
-    let cookie = conn.send_request_checked(&xcb::x::PutImage {
-        drawable: x::Drawable::Pixmap(bg_id),
-        gc: gcon_id,
-        width: (src_width / divide_factor) as u16,
-        height: (src_height / divide_factor) as u16,
-        dst_x: 0,
-        dst_y: 0,
-        left_pad: 0,
-        depth: 24,
-        format: x::ImageFormat::ZPixmap,
-        data: &rgba_to_bgra(bg_image_data),
-    });
-
-    let checked = conn.check_request(cookie);
-    if checked.is_err() {
-        println!("Error putting image on pixmap");
-        println!("{:?}", checked);
-    }
-
-    // calculate the amount of times to multiply the image by
-    let transform = [
-        1, 0, 0,
-        0, 1, 0,
-        0, 0, divide_factor as i32,
-    ];
-
-    // create picture from pixmap
-    let pic_id = conn.generate_id();
-    let cookie = conn.send_request_checked(&xcb::render::CreatePicture {
-        pid: pic_id,
-        drawable: x::Drawable::Pixmap(bg_id),
-        format: pict_format,
-        value_list: &[],
-    });
-    let checked = conn.check_request(cookie);
-    if checked.is_err() {
-        println!("Error creating picture");
-    }
-
-    // set picture transform
-    let cookie = conn.send_request_checked(&xcb::render::SetPictureTransform {
-        picture: pic_id,
-        transform: xcb::render::Transform {
-            matrix11: transform[0],
-            matrix12: transform[1],
-            matrix13: transform[2],
-            matrix21: transform[3],
-            matrix22: transform[4],
-            matrix23: transform[5],
-            matrix31: transform[6],
-            matrix32: transform[7],
-            matrix33: transform[8],
-        },
-    });
-
-    let checked = conn.check_request(cookie);
-    if checked.is_err() {
-        println!("Error setting picture transform");
-    }
-
-    // get picture of window
-    let desktop_pic_id = conn.generate_id();
-    let cookie = conn.send_request_checked(&xcb::render::CreatePicture {
-        pid: desktop_pic_id,
-        drawable: x::Drawable::Window(desktop_id),
-        format: pict_format,
-        value_list: &[],
-    });
-
-    let checked = conn.check_request(cookie);
-    if checked.is_err() {
-        println!("Error creating picture");
-    }
-
-    // composite picture onto window
-    redraw_desktop(&conn, pic_id, desktop_pic_id, src_width, src_height);
-
-    // map the window
-    let cookie = conn.send_request_checked(&xcb::x::MapWindow {
-        window: desktop_id,
-    });
-
-    let checked = conn.check_request(cookie);
-    if checked.is_err() {
-        println!("Error mapping window");
-    }
+    let desktop_id = setup_desktop(&conn, screen.root_visual(), pict_format, gcon_id, root, src_width, src_height);
 
     // grab pointer for drawing the cursor
     let cookie = conn.send_request(&xcb::x::GrabPointer {
